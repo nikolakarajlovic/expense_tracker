f8d7e7b472c4f39f8bf65afb311f905ff1804033
nikolakarajlovic <95770099+nikolakarajlovic@users.noreply.github.com>
2023-12-13 23:24:46 +0100

    015

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,10 +10,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -21,7 +23,7 @@ class _NewExpenseState extends State<NewExpense> {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(children:  [
+        child: Column(children: [
           TextField(
             controller: _titleController,
             maxLength: 50,
@@ -29,8 +31,25 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
-          Row(children:[
-            ElevatedButton(onPressed: (){print(_titleController.text);}, child: const Text('Save Expense'))
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
+          Row(children: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text('Save Expense'))
           ])
         ]));
   }
